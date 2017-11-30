4124b3352f43bcab101d510a5c81fc4f7ee0fce6
Gonzalo Tercero <gtercerogarcia@gmail.com>
2017-11-30 19:08:48 +0100

    preparado para aceptar peticiones POST

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 
-$array['status'] = 'OK';
-$array['result'] = 250;
 
 
-echo json_encode($array);
+
+if(!empty($_POST)){
+    $array['status'] = 'OK';
+    $array['result'] = 250;
+    $array['msg'] = "Es una petición POST";    
+
+    echo json_encode($array);
+    return;
+}
+
+if(!empty($_GET)){
+    $array['status'] = 'KO';
+    $array['result'] = 0;
+    $array['msg'] = "no deberías estar aquí";    
+
+    echo json_encode($array);
+    return;
+}
+
